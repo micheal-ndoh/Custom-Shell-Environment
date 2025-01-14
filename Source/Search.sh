f4b2bc8b345cb71a7011f85f4f05be77204eaea2
Micheal Ndoh <michaelndoh9@gmail.com>
2025-01-14 23:35:53 +0100

    added wc to search.sh

--- Source/Search.sh
+++ Source/Search.sh
@@ -1,19 +1,18 @@
-echo "  # Function to search for files on the entire system in a case-insensitive manner and count lines in each file
+# Function to search for files on the entire system in a case-insensitive manner and count lines in each file
 search() {
-    if [ #$ -ne 1 ]; then
-        echo Usage: search filename
+    if [ "$#" -ne 1 ]; then
+        echo "Usage: search filename"
         return 1
     fi
 
-    filename=
+    filename=$1
 
     # Search for files on the entire system in a case-insensitive manner
-    find / -type f -iname  2>/dev/null | while read file_path; do
-        echo File: 
+    find / -type f -iname "$filename" 2>/dev/null | while read file_path; do
+        echo "File: $file_path"
         # Count the lines in the file
-        line_count=
-        echo The file has lines.
+        line_count=$(wc -l < "$file_path")
+        echo "The file has $line_count lines."
         echo # Empty line for better readability
     done
-}   
- "  >> ~/.zshrc
+}
